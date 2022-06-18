ef06d8b86fdd5ef655fa9a368fa3261652a56d8a
thanat raktham <thanat.sun19281@gmail.com>
2022-06-18 23:49:02 +0700

    prepare run build

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,20 +1,21 @@
 import React from "react";
-import "../assets/styles/Landing.css";
-import { Button } from "react-bootstrap";
-import ImageBoomSeen from "../assets/image/boomseen.png";
-import dice from "../assets/image/dice.png";
+// import "../assets/styles/Landing.css";
+// import { Button } from "react-bootstrap";
+// import ImageBoomSeen from "../assets/image/boomseen.png";
+// import dice from "../assets/image/dice.png";
 
 function Landing() {
   return (
-    <div className="container">
-      <img src={ImageBoomSeen} alt="ImageBoomSeen" className="ImageBoomSeen" />
-      <img src={dice} alt="dice" className="dice" />
+    // <div className="container">
+    //   <img src={ImageBoomSeen} alt="ImageBoomSeen" className="ImageBoomSeen" />
+    //   <img src={dice} alt="dice" className="dice" />
 
-      <div className="head-body">
-      <h1> LEADER BOARD SCORE </h1>
-      </div>
-      <Button className="leader-btn">LEADERBOARD</Button>
-    </div>
+    //   <div className="head-body">
+    //   <h1> LEADER BOARD SCORE </h1>
+    //   </div>
+    //   <Button className="leader-btn">LEADERBOARD</Button>
+    // </div>
+    <div></div>
   );
 }
 
