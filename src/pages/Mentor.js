737c47c93392defcd1a718324e0799f41ae1fc14
wittawat-jump <watz_123@hotmail.co.th>
2022-06-23 23:23:19 +0700

    Mentor Final

--- src/pages/Mentor.js
+++ src/pages/Mentor.js
@@ -5,13 +5,13 @@ import MentorTable from "../components/MentorTable";
 import Box from "@mui/material/Box";
 import { Stack, Typography } from "@mui/material";
 import SplashScreen from "../components/SplashScreen";
+import color from "../constant/color";
 import limitStringLength from "../components/Functions/limitStringLength";
 
 export default function Mentor() {
   const { projectNameParams, mentorNameParams } = useParams();
   console.log(mentorNameParams);
   const [project, setProject] = React.useState();
-
   const queryProject = async (projectName) => {
     await db
       .collection("users")
@@ -45,32 +45,80 @@ export default function Mentor() {
           height={"100%"}
           alignItems="center"
           justifyContent="space-between"
-          paddingX={"50px"}
+          padding={"2%"}
         >
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              width: "100%",
-              minWidth: 350,
-              height: 200,
-              justifyContent: "center",
-              marginRight: "0px",
-              backgroundColor: "black",
-              borderRadius: 10,
-              color: "white",
-            }}
+          <Stack
+            display={"flex"}
+            flexDirection={"row"}
+            height={200}
+            width={"100%"}
+            alignItems={"center"}
+            paddingX={"1%"}
           >
+            <Box
+              component={"img"}
+              src={project?.imageUrl}
+              alt={"not found"}
+              sx={{ width: 200, height: 200, marginRight: "30px" }}
+            />
             <Typography
-              variant={"h1"}
               sx={{
-                textAlign: "center",
-                color: "white",
+                fontSize: 60,
+                fontWeight: 200,
+                fontFamily: "Russo One",
+                marginRight: "30px",
               }}
             >
-              {limitStringLength(mentorNameParams, 29)}
+              LEADERBOARD
             </Typography>
-          </Box>
+            <Box
+              sx={{
+                width: 10,
+                height: "40%",
+                marginRight: "30px",
+                backgroundColor: color.primaryOrange,
+              }}
+            />
+            <Typography
+              sx={{
+                fontSize: 32,
+                fontWeight: 900,
+                marginRight: "30px",
+                paddingTop: "30px",
+                color: color.secondaryGrey,
+              }}
+            >
+              PROJECT {project?.projectName.toUpperCase()}{" "}
+            </Typography>
+            <Stack
+              sx={{
+                flexGrow: 1,
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "flex-end",
+              }}
+            >
+              <Stack
+                sx={{
+                  width: 450,
+                  height: 90,
+                  backgroundColor: "white",
+                  borderRadius: "30px",
+                  justifyContent: "center",
+                }}
+              >
+                <Typography
+                  sx={{
+                    marginLeft: "20px",
+                    justifyContent: "center",
+                    fontSize: 38,
+                  }}
+                >
+                  Mentor : {limitStringLength(mentorNameParams, 29)}
+                </Typography>
+              </Stack>
+            </Stack>
+          </Stack>
           <Box
             sx={{
               height: "75%",
