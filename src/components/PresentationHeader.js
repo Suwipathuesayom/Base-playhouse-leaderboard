af81ad7da13a88f7b55aa9ba9c737e7387f0702f
thanat raktham <thanat.sun19281@gmail.com>
2022-06-24 21:18:18 +0700

    merge presentationHeader conflict

--- src/components/PresentationHeader.js
+++ src/components/PresentationHeader.js
@@ -1,7 +1,6 @@
 import { Box, Stack, Typography, useMediaQuery, useTheme } from "@mui/material";
 import color from "../constant/color";
 
-<<<<<<< HEAD
 const PresentationHeader = ({ project }) => {
   const theme = useTheme();
   const smallScreen = useMediaQuery(theme.breakpoints.down("sm"));
@@ -69,53 +68,5 @@ const PresentationHeader = ({ project }) => {
     </Stack>
   );
 };
-=======
-const PresentationHeader = ({ project }) => (
-  <Stack
-    display={"flex"}
-    flexDirection={"row"}
-    height={200}
-    alignItems={"center"}
-    paddingX={"1%"}
-    // backgroundColor={"red"}
-  >
-    <Box
-      component={"img"}
-      src={project?.imageUrl}
-      alt={"not found"}
-      sx={{ width: 90, height: 90, marginRight: "30px" }}
-    />
-    <Typography
-      sx={{
-        fontSize: 20,
-        fontWeight: 200,
-        fontFamily: "Russo One",
-        marginRight: "30px",
-      }}
-    >
-      LEADERBOARD
-    </Typography>
-    <Box
-      sx={{
-        width: 10,
-        height: "40%",
-        marginRight: "30px",
-        backgroundColor: color.primaryOrange,
-      }}
-    />
-    <Typography
-      sx={{
-        fontSize: 22,
-        fontWeight: 500,
-        // marginRight: "10px",
-        paddingTop: "30px",
-        color: color.secondaryGrey,
-      }}
-    >
-      {project?.projectName.toUpperCase()}
-    </Typography>
-  </Stack>
-);
->>>>>>> FOUR
 
 export default PresentationHeader;
