331c67f968ac2880d98277ea2973202c834bd888
wittawat-jump <watz_123@hotmail.co.th>
2022-07-26 13:58:56 +0700

    Leaderboard edit V2

--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -1,13 +1,44 @@
 import React from "react";
+import "./Leaderboard.css";
 
 const Leaderboard = () => {
   return (
-    <div>
-      <div>LOGO</div>
-      <div>Base</div>
-      <div>Leaderboard</div>
-      <div>image</div>
-      <div>project-name</div>
+    <div className="leaderboard-container">
+      <div className="navbar-container">
+        <img
+          src="https://yt3.ggpht.com/ytc/AKedOLR_UA-QGw_Oe2-RZ6o-egVZQ-Tk5my0qxZfFAvZkg=s900-c-k-c0x00ffffff-no-rj"
+          alt="not found img"
+        />
+        <h2>BASE</h2>
+      </div>
+
+      <div>
+        <div>Leaderboard</div>
+        <div>image</div>
+        <div>project-name</div>
+      </div>
+
+      <div>
+        <div>แท่ง1</div>
+        <div>แท่ง2</div>
+        <div>แท่ง3</div>
+      </div>
+
+      <div>
+        Table Body
+        <div>Rank</div>
+        <div>Group</div>
+        <div>Name</div>
+        <div>Total</div>
+      </div>
+
+      <div>
+        Table Task
+        <div>
+          Header
+          <div>Point Task</div>
+        </div>
+      </div>
     </div>
   );
 };
