b127706f7a71ecc4d4d270ced7799a93edd669d6
DINOSUN <4fournatcha@gmail.com>
2022-07-26 13:02:15 +0700

    Leaderboard_ui

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { ThemeProvider } from "@mui/material";
 import "./App.css";
 import theme from "./assets/theme/theme";
 import RootNavigation from "./navigation/RootNavigation";
+import Leaderboard from "./pages/Leaderboard";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <RootNavigation />
+      {/* <RootNavigation /> */}
+      <Leaderboard></Leaderboard>
     </ThemeProvider>
   );
 }
---
+++ src/pages/Leaderboard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Leaderboard = () => {
+  return (
+    <div>
+      <div>LOGO</div>
+      <div>Base</div>
+      <div>Leaderboard</div>
+      <div>image</div>
+      <div>project-name</div>
+    </div>
+  );
+};
+
+export default Leaderboard;
