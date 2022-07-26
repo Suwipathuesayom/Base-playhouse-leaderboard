d8249ddfa63d55db0a49d86748af4eb7ec836099
Suwipathuesayom <suwipa.thu@gmail.com>
2022-07-26 22:12:23 +0700

    config

--- src/pages/Learner.js
+++ src/pages/Learner.js
@@ -8,11 +8,7 @@ import { useMediaQuery, useTheme } from "@mui/material";
 import { useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import LearnerBox from "../components/LearnerBox";
-<<<<<<< HEAD
-import SpeakerBox from "./../components/SpeakerBox";
-=======
 import SpeakerBox from "../components/SpeakerBox";
->>>>>>> main2
 
 const Learner = () => {
   const [project, setProject] = useState({});
@@ -22,53 +18,6 @@ const Learner = () => {
 
   if (!!Object.keys(project).length) {
     return (
-<<<<<<< HEAD
-      <div className="App">
-        <Navbar header={`GROUP ${groupNameParams ? groupNameParams : ""}`} />
-        <PresentationHeader project={project} />
-        <div className="learner__container">
-          <div className="learner__header">
-            <h3>RANK</h3>
-            {!smallScreen && <img alt="" />}
-            {!smallScreen && <h3>GROUP</h3>}
-            <h2>NAME</h2>
-            <h3>TOTAL</h3>
-          </div>
-          <FlipMove className="learner__flipMove">
-            {project?.learnerGroups?.map((group, rankIndex) => (
-              <LearnerBox
-                key={group.groupIndex}
-                rankIndex={rankIndex}
-                theme={project?.theme}
-                {...group}
-                smallScreen={smallScreen}
-                groupNameParams={groupNameParams}
-              />
-            ))}
-          </FlipMove>
-          <div className="speaker__speakerContent">
-            <div className="speaker__speakerHeader">
-              {project.tasks
-                .filter((task) => !task.isHidden)
-                .map((task, taskIndex) => (
-                  <h3 key={taskIndex}>{task.taskName}</h3>
-                ))}
-            </div>
-            <FlipMove>
-              {project?.learnerGroups?.map((group, rankIndex) => (
-                <SpeakerBox
-                  key={group.groupIndex}
-                  rankIndex={rankIndex}
-                  theme={project?.theme}
-                  {...group}
-                  smallScreen={smallScreen}
-                  groupNameParams={"null"}
-                  project={project}
-                />
-              ))}
-            </FlipMove>
-          </div>
-=======
       <>
         <div className="App">
           <Navbar header={`GROUP ${groupNameParams ? groupNameParams : ""}`} />
@@ -140,7 +89,6 @@ const Learner = () => {
               </div>
             </div>
           )}
->>>>>>> main2
         </div>
       </>
     );
